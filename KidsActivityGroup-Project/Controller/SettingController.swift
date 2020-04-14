21ee8cf2af1b067d5bb609bedf4db47e222b80c2
uwagboe <sm.uwagboe@gmail.com>
2020-04-14 16:37:30 -0400

    added the corresponding controllers and tab bar controller

---
+++ KidsActivityGroup-Project/Controller/SettingController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingController.swift
+//  KidsActivityGroup-Project
+//
+//  Created by Pursuit on 4/14/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class SettingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
