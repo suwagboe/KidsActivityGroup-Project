e2fcf78576e34a26d3f07e8ea2f48f4376862856
Juan C <juanceballos@pursuit.org>
2020-04-15 16:54:14 -0400

    some ui on vc1, change filename

---
+++ KidsActivityGroup-Project/Controller/ScheduleViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  KidsActivityGroup-Project
+//
+//  Created by Pursuit on 4/14/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+    }
+    
+    private func configureCollectionView()   {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+}
+
+extension ScheduleViewController: UICollectionViewDataSource    {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "scheduleCell", for: indexPath)
+        cell.backgroundColor = .systemTeal
+        return cell
+    }
+    
+}
+
+extension ScheduleViewController: UICollectionViewDelegateFlowLayout    {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.40
+        
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
+
