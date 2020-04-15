19a0580b3b1541683f4b53e212572f4be291cb3e
Kelby Mittan <kelbymittan@pursuit.org>
2020-04-15 19:33:28 -0400

    core data entity added

---
+++ KidsActivityGroup-Project/Core Data/CDActivity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CDActivity+CoreDataClass.swift
+//  KidsActivityGroup-Project
+//
+//  Created by Kelby Mittan on 4/15/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CDActivity: NSManagedObject {
+
+}
---
+++ KidsActivityGroup-Project/Core Data/CDActivity+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  CDActivity+CoreDataProperties.swift
+//  KidsActivityGroup-Project
+//
+//  Created by Kelby Mittan on 4/15/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CDActivity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDActivity> {
+        return NSFetchRequest<CDActivity>(entityName: "CDActivity")
+    }
+
+    @NSManaged public var videoData: Data?
+    @NSManaged public var imageData: Data?
+    @NSManaged public var activityDate: Date?
+    @NSManaged public var activityId: String?
+    @NSManaged public var activityDescription: String?
+
+}
