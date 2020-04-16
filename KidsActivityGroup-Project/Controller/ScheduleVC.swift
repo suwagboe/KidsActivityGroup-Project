0afdf48e91bc3ca20170fa822a37d11093414463
Juan C <juanceballos@pursuit.org>
2020-04-16 11:09:31 -0400

    update to schedulevc prepping for coredata

--- KidsActivityGroup-Project/Controller/ScheduleVC.swift
+++ KidsActivityGroup-Project/Controller/ScheduleVC.swift
@@ -9,40 +9,52 @@
 import UIKit
 
 class ScheduleVC: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private var plannedActivities = [Activity]()
+    // refactor to CoreDataObject with title description nil image
+    // update when selected to add image to CDObject stored
     
-      override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-      }
-      private func configureCollectionView()  {
+    }
+    
+    private func configureCollectionView()  {
         collectionView.dataSource = self
         collectionView.delegate = self
-      }
     }
+}
 
 extension ScheduleVC: UICollectionViewDataSource  {
-      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
-      }
-      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "scheduleCell", for: indexPath)
-        cell.backgroundColor = .systemTeal
-        return cell
-      }
     }
-    extension ScheduleVC: UICollectionViewDelegateFlowLayout  {
-      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "scheduleCell", for: indexPath)
+        //cell.backgroundColor = .systemTeal
+        return cell
+    }
+    
+}
+
+extension ScheduleVC: UICollectionViewDelegateFlowLayout  {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("pressed a cell \(indexPath.row)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let maxSize: CGSize = UIScreen.main.bounds.size
         let itemWidth: CGFloat = maxSize.width
         let itemHeight: CGFloat = maxSize.height * 0.40
         return CGSize(width: itemWidth, height: itemHeight)
-      }
-      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-      }
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+}
 
-  
+
